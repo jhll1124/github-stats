8a15c5ca25295b92cc43b530ddb048293bd9f004
忘忧北萱草 <wybxc@qq.com>
2022-12-08 15:05:47 +0800

    update dependencies

--- src/optimizers/hydration-style.ts
+++ src/optimizers/hydration-style.ts
@@ -1,8 +1,8 @@
-import { globalStyle } from "../common/Style.tsx";
-import { minify } from "csso";
+import { globalStyle } from '../common/Style.tsx';
+import { minify } from 'csso';
 
 export default function hydrationStyle(svg: string) {
-  const style = globalStyle.join("\n");
-  const minified = minify(style);
-  return svg.replace("#_STYLE_PLACEHOLDER_#", minified.css);
+  const style = globalStyle.join('\n');
+  const minified = minify(style, {});
+  return svg.replace('#_STYLE_PLACEHOLDER_#', minified.css);
 }
